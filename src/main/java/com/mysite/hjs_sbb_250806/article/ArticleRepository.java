a5870d5331ad11f9fc17f662390235ffc6e8d4ef
jeongseong <hjs54336297@gmail.com>
2025-08-06 14:45:32 +0900

    make repository classes

--- src/main/java/com/mysite/hjs_sbb_250806/article/ArticleRepository.java
+++ src/main/java/com/mysite/hjs_sbb_250806/article/ArticleRepository.java
@@ -1,4 +1,6 @@
 package com.mysite.hjs_sbb_250806.article;
 
-public class ArticleRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository extends JpaRepository<Article, Integer> {
 }
---
+++ src/main/java/com/mysite/hjs_sbb_250806/coment/ComentRepository.java
@@ -0,0 +1,6 @@
+package com.mysite.hjs_sbb_250806.coment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ComentRepository extends JpaRepository<Coment, Integer> {
+}
