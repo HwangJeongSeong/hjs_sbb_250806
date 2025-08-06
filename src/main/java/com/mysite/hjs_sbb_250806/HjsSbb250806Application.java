145e0641b19f4674c5e85b30a42a5c74c5195434
jeongseong <hjs54336297@gmail.com>
2025-08-06 14:15:41 +0900

    Initial commit

---
+++ src/main/java/com/mysite/hjs_sbb_250806/HjsSbb250806Application.java
@@ -0,0 +1,13 @@
+package com.mysite.hjs_sbb_250806;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HjsSbb250806Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HjsSbb250806Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysite/hjs_sbb_250806/HjsSbb250806ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.hjs_sbb_250806;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HjsSbb250806ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
